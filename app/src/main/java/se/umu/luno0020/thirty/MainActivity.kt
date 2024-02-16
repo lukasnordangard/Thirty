d798974f869221982987e2f42922cf44fc19054f
lukasnordangard <lukasnordangard@fastmail.fm>
2024-02-16 12:43:29 +0100

    Removed rotation lock and added landscape views

--- app/src/main/java/se/umu/luno0020/thirty/MainActivity.kt
+++ app/src/main/java/se/umu/luno0020/thirty/MainActivity.kt
@@ -18,7 +18,7 @@ import com.google.android.material.textfield.TextInputLayout
  * and navigation to the result screen.
  *
  * @author Lukas Nordangård id20lsd
- * @version 1.0
+ * @version 2.0
  */
 class MainActivity : AppCompatActivity() {
 
